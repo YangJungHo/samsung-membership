528bb46a5135fc289028b8158980c4a688745002
YangJungHo <yjh85@kumoh.ac.kr>
2012-07-23 06:57:24 +0900

    TEST

--- SControll/gen/com/example/scontroll/R.java
+++ SControll/gen/com/example/scontroll/R.java
@@ -16,13 +16,24 @@ public final class R {
         public static final int padding_small=0x7f040000;
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int rose=0x7f020002;
-        public static final int scontroll=0x7f020003;
+        public static final int background=0x7f020000;
+        public static final int background2=0x7f020001;
+        public static final int ic_action_search=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int large=0x7f020004;
+        public static final int needle=0x7f020005;
+        public static final int rose=0x7f020006;
+        public static final int rose_1=0x7f020007;
+        public static final int scontrol=0x7f020008;
+        public static final int scontroll=0x7f020009;
+        public static final int scontrolll=0x7f02000a;
     }
     public static final class id {
-        public static final int menu_settings=0x7f080000;
+        public static final int SeekBar2=0x7f080001;
+        public static final int SeekBar3=0x7f080002;
+        public static final int SeekBar4=0x7f080003;
+        public static final int menu_settings=0x7f080004;
+        public static final int seekBar1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- SControll/src/com/example/scontroll/MainActivity.java
+++ SControll/src/com/example/scontroll/MainActivity.java
@@ -13,32 +13,42 @@ import android.os.Handler;
 import android.os.Message;
 import android.view.Menu;
 import android.view.View;
-import android.view.Window;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
 
 
 
 public class MainActivity extends Activity {
 	
 	
-	//TextView speed;
-	//SeekBar testbar;
-	
-	
+	SeekBar testbar1,testbar2,testbar3,testbar4;
+	int velocityAngle = -130;
+	int batteryAngle = -120;
+	int fuelAngle = 120;
+	int coolantAngle = -145;
 		
-    @Override
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);   // 프로그램의 타이틀 없애기
-        //setContentView(R.layout.activity_main);
-        MyView vw = new MyView(this);
-		setContentView(vw);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);   // 프로그램의 타이틀 없애기
+		setContentView(R.layout.activity_main);
 		
-   
-        /*speed =(TextView)findViewById(R.id.speedtext);
+		MyView vw = new MyView(this);
+		fuel fuelGauge = new fuel(this);
+		Battery batteryGauge = new Battery(this);
+		coolant coolantGauge = new coolant(this);    
+		addContentView(vw, new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT) );  
+		addContentView(batteryGauge, new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT) );  
+		addContentView(fuelGauge, new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT) );  
+		addContentView(coolantGauge, new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT) );  
         
-        testbar =(SeekBar)findViewById(R.id.test);
         
-        testbar.setOnSeekBarChangeListener(
+		testbar1 = (SeekBar)findViewById(R.id.seekBar1);
+		testbar2 = (SeekBar)findViewById(R.id.SeekBar2);
+		testbar3 = (SeekBar)findViewById(R.id.SeekBar3);
+		testbar4 = (SeekBar)findViewById(R.id.SeekBar4);
+        testbar1.setOnSeekBarChangeListener(
         		new SeekBar.OnSeekBarChangeListener() {
 			
 			public void onStopTrackingTouch(SeekBar seekBar) {
@@ -51,20 +61,86 @@ public class MainActivity extends Activity {
 				
 			}
 			
-			public void onProgressChanged(SeekBar seekBar, int progress,
-					boolean fromUser) {
+			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 				// TODO Auto-generated method stub
-				speed.setText(progress+"");
+				velocityAngle = ((progress*3)-130);
+				if(velocityAngle >=131){
+					velocityAngle =130;
+				}
+				
+			}
+		});
+        testbar2.setOnSeekBarChangeListener(
+        		new SeekBar.OnSeekBarChangeListener() {
+			
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
 				
 			}
 			
-
-		});*/
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+				// TODO Auto-generated method stub
+				batteryAngle = ((progress)-120);
+				if(batteryAngle >=-30){
+					batteryAngle =-30;
+				}
+				
+			}
+		});
+        testbar3.setOnSeekBarChangeListener(
+        		new SeekBar.OnSeekBarChangeListener() {
+			
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+				// TODO Auto-generated method stub
+				fuelAngle = ((-progress)+120);
+				if(fuelAngle <=25){
+					fuelAngle =25;
+				}
+				
+			}
+		});
+        testbar4.setOnSeekBarChangeListener(
+        		new SeekBar.OnSeekBarChangeListener() {
+			
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+				// TODO Auto-generated method stub
+				coolantAngle = ((-progress)-145);
+				if(coolantAngle <=-215){
+					coolantAngle =-215;
+				}
+				
+			}
+		});
     }
     
     protected class MyView extends View {
     	
-    	int count=0;
+    	int count = 0;
 
 		public MyView(Context context) {
              super(context);
@@ -73,49 +149,135 @@ public class MainActivity extends Activity {
         }
     
         public void onDraw(Canvas canvas) {
-             int cx = getWidth() / 2;       // View 의 중심점
-             int cy = getHeight() / 2;
-             int w = 0;                 
-             int h = 0;
-   
-     
-             Bitmap bitmap = null;
-             Paint paint = new Paint();
-             paint.setColor(Color.RED);                
-             paint.setStyle(Paint.Style.STROKE);    // 외곽선만 그린다
-             canvas.drawColor(Color.TRANSPARENT);        // View를 흰색으로 채우기
-             bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.rose);
-             w = bitmap.getWidth() / 2;          // 회전축 계산
-             h = bitmap.getHeight();
 
-     
-         canvas.drawBitmap(bitmap, 10, 10, null);                   // 10, 10 위치에 원래의 이미지 출력
-         canvas.drawCircle(w + 10, h + 10, 10, paint);             // 회전축을 빨간 원으로 표시
-         int n=10;
-         n+=count;
-         canvas.rotate(n, cx, cy);                                  // Canvas를 중심점을 회전축으로 20도 회전 
-         canvas.drawBitmap(bitmap, cx - w, cy - h, null);    // 중심점에 이미지 끝이 오도록 출력
-         count++;
+             int CenterX = getWidth() / 2;	// X오리진
+             int CenterY = (getHeight() / 2) - 100;	//	Y오리진
+             int NeedleWidth  = 0;                 
+             int NeedleHeight = 0;
+             int GaugeWidth  = 0;
+             int GaugeHeight = 0;
+
+             Paint paint = new Paint();
+             paint.setColor(Color.RED);	// 회전축(원) 표시색 RED                 
+             paint.setStyle(Paint.Style.STROKE);	// 회전축(원) 외곽선만 그린다.
+             
+             Bitmap Needle = BitmapFactory.decodeResource(getResources(), R.drawable.needle);
+             Bitmap backGround  = BitmapFactory.decodeResource(getResources(), R.drawable.background2);
+             Bitmap NeedleResize = Bitmap.createScaledBitmap(Needle, 40, 140, true);
+             Bitmap GaugeResize  = Bitmap.createScaledBitmap(backGround, 370, 370, true);
+             
+             NeedleWidth  = NeedleResize.getWidth() / 2;	// 회전축 계산
+             NeedleHeight = NeedleResize.getHeight();
+             GaugeWidth  = GaugeResize.getWidth() / 2;
+             GaugeHeight = GaugeResize.getHeight() / 2;
+             
+             canvas.drawBitmap(GaugeResize, CenterX - GaugeWidth, CenterY - GaugeHeight, null); //	속도게이지 배치 
+             canvas.drawCircle(CenterX, CenterY, 120, paint);	// 회전축을 빨간 원으로 표시
+             canvas.rotate(velocityAngle, CenterX, CenterY);	// Canvas를 중심점을 회전축으로 angle각도만큼  회전 
+             canvas.drawBitmap(NeedleResize, CenterX - NeedleWidth, CenterY -(NeedleHeight+50), null);	// 중심점에 이미지 끝이 오도록 출력
+             
+
+             
 
       } // onDraw
         
-        Handler timer = new Handler(){
-
-			public void  handleMessage(Message msg) {
-				invalidate();
-				timer.sendEmptyMessageDelayed(0, 10);
-			}
-        };// Handler 
         
+        Handler timer = new Handler() {               // 타이머로 사용할 Handler
+            public void handleMessage(Message msg) {
+                     invalidate();                                       // onDraw() 다시 실행
+                     timer.sendEmptyMessageDelayed(0, 10);      // 10/1000초마다 실행
+            }
+        }; // Handler
      } // class */
-
     
-
-	
-
+    protected class Battery extends View{
+    	
+    	public Battery(Context context) {
+            super(context);
+       }    	
+    	public void onDraw(Canvas canvas){
+    		
+    		int CenterX = (getWidth() / 2)-397;
+            int CenterY = (getWidth() / 2)-442;
+            int NeedleWidth  = 0;
+            int NeedleHeight = 0;
+            
+            Paint paint = new Paint();
+            paint.setColor(Color.RED);	// 회전축(원) 표시색 RED                 
+            paint.setStyle(Paint.Style.STROKE);	// 회전축(원) 외곽선만 그린다.
+            
+            Bitmap velocityNeedle = BitmapFactory.decodeResource(getResources(), R.drawable.needle);
+            Bitmap NeedleResize = Bitmap.createScaledBitmap(velocityNeedle, 20, 25, true);
+            
+            NeedleWidth  = NeedleResize.getWidth() / 2;
+            NeedleHeight = NeedleResize.getHeight() /2;
+            
+            canvas.drawCircle(CenterX, CenterY, 44, paint);	// 회전축을 빨간 원으로 표시
+            canvas.rotate(batteryAngle, CenterX, CenterY);	// Canvas를 중심점을 회전축으로 angle각도만큼  회전 
+            canvas.drawBitmap(NeedleResize, CenterX - NeedleWidth, CenterY - (NeedleHeight+130), null); //	속도게이지 배치
+    	}
+    	
+    }// class
     
+    protected class fuel extends View{
+    	
+    	public fuel(Context context) {
+            super(context);
+       }    	
+    	public void onDraw(Canvas canvas){
+    		
+    		int CenterX = (getWidth() / 2)-397;
+            int CenterY = (getWidth() / 2)-442;
+            int NeedleWidth  = 0;
+            int NeedleHeight = 0;
+            
+            Paint paint = new Paint();
+            paint.setColor(Color.RED);	// 회전축(원) 표시색 RED                 
+            paint.setStyle(Paint.Style.STROKE);	// 회전축(원) 외곽선만 그린다.
+            
+            Bitmap velocityNeedle = BitmapFactory.decodeResource(getResources(), R.drawable.needle);
+            Bitmap NeedleResize = Bitmap.createScaledBitmap(velocityNeedle, 20, 25, true);
+            
+            NeedleWidth  = NeedleResize.getWidth() / 2;
+            NeedleHeight = NeedleResize.getHeight() /2;
+            
+            canvas.drawCircle(CenterX, CenterY, 44, paint);	// 회전축을 빨간 원으로 표시
+            canvas.rotate(fuelAngle, CenterX, CenterY);	// Canvas를 중심점을 회전축으로 angle각도만큼  회전 
+            canvas.drawBitmap(NeedleResize, CenterX - NeedleWidth, CenterY - (NeedleHeight+130), null); //	속도게이지 배치
+    	}
+    	
+    }// class
     
-       
+    protected class coolant extends View{
+    	
+    	public coolant(Context context) {
+            super(context);
+       }    	
+    	public void onDraw(Canvas canvas){
+    		
+    		int CenterX = (getWidth() / 2)-397;
+            int CenterY = (getWidth() / 2)-442;
+            int NeedleWidth  = 0;
+            int NeedleHeight = 0;
+            
+            Paint paint = new Paint();
+            paint.setColor(Color.RED);	// 회전축(원) 표시색 RED                 
+            paint.setStyle(Paint.Style.STROKE);	// 회전축(원) 외곽선만 그린다.
+            
+            Bitmap velocityNeedle = BitmapFactory.decodeResource(getResources(), R.drawable.needle);
+            Bitmap NeedleResize = Bitmap.createScaledBitmap(velocityNeedle, 20, 25, true);
+            
+            NeedleWidth  = NeedleResize.getWidth() / 2;
+            NeedleHeight = NeedleResize.getHeight() /2;
+            
+            canvas.drawCircle(CenterX, CenterY, 44, paint);	// 회전축을 빨간 원으로 표시
+            canvas.rotate(coolantAngle, CenterX, CenterY);	// Canvas를 중심점을 회전축으로 angle각도만큼  회전 
+            canvas.drawBitmap(NeedleResize, CenterX - NeedleWidth, CenterY - (NeedleHeight+130), null); //	속도게이지 배치
+    	}
+    	
+    }// class
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
